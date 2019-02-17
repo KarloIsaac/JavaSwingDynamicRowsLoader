dc0aac9b2c0862150c10b0d2fc0e58d5b7b17b21
KarloIsaac <karlo.ijc@gmail.com>
2019-02-17 15:29:45 -0600

    Updated the documentation of the DisplayUpdateTaskOverListener interface

--- DynamicRowsLoaderWorkspace/RowsLoaderProject/production/mx/gob/cofepris/cos/deds/dynamicRowsLoader/DisplayUpdateTaskOverListener.java
+++ DynamicRowsLoaderWorkspace/RowsLoaderProject/production/mx/gob/cofepris/cos/deds/dynamicRowsLoader/DisplayUpdateTaskOverListener.java
@@ -1,7 +1,17 @@
 package mx.gob.cofepris.cos.deds.dynamicRowsLoader;
 
+/**
+ * Interface to be implemented by all parties interested on being informed of the completion
+ * of the {@link MultiRowViewRenditionManager#scheduleUpdateVisibleComponentsTask 
+ * GUI updating task}
+ * 
+ * @author kijimenez
+ *
+ */
 public interface DisplayUpdateTaskOverListener {
 
-	
+	/**
+	 * The actions to perform after the GUI has been updated.
+	 */
 	public void performUpdateTaskIsOverActions();
 }
